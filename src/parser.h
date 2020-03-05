2057fd09ed283f4312a1c5b9e5da09b372b4d578
Benjamin LECOMTE <benjamin.lecomte@etu.esisar.grenoble-inp.fr>
2020-03-05 14:41:45 +0100

    La fonction des regex / exemple avec la fonction DIGIT

--- src/parser.h
+++ src/parser.h
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex.h>
 
 #define FALSE 0
 #define TRUE 1
 
-int DIGIT(const char* c);
-int etoile(int (*fonction)(const char* c), const char* s, int mini, int maxi);
+int regexTest(const char* string, const char* regex, const int size);
+int DIGIT(const char* s);
+int etoile(int (*fonction)(const char* s), const char* s, int mini, int maxi);
