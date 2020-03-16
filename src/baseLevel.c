a115cc45ab06d281faa530abf6b8560696ecfca6
Benjamin LECOMTE <benjamin.lecomte@etu.esisar.grenoble-inp.fr>
2020-03-16 11:34:16 +0100

    Tout est un peu plus propre

--- src/baseLevel.c
+++ src/baseLevel.c
@@ -4,248 +4,251 @@
 
 //Définitions de base de l'ABNF
 int DIGIT(const char* s, Node* node) {
-	int toReturn = FALSE;
-    if(regexTest(s,"[0-9]",1)){
-    	if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-        toReturn =  TRUE;
-    } else {
-        removeNode(node);
+	int toReturn = regexTest(s,"[0-9]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
     }
     return toReturn;
 }
-int ALPHA(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"[A-Za-z]",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int ALPHA(const char* s, Node* node) {
+	int toReturn = regexTest(s,"[A-Za-z]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int BIT(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"[01]",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int BIT(const char* s, Node* node) {
+	int toReturn = regexTest(s,"[01]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int HEXDIG(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"[0-9A-Fa-f]",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int HEXDIG(const char* s, Node* node) {
+	int toReturn = regexTest(s,"[0-9A-Fa-f]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int CHAR_(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"[-]",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int CHAR_(const char* s, Node* node) {
+	int toReturn = regexTest(s,"[-]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int CR(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"\r",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int CR(const char* s, Node* node) {
+	int toReturn = regexTest(s,"\r",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int LF(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"\n",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int LF(const char* s, Node* node) {
+	int toReturn = regexTest(s,"[\n]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int DQUOTE(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"\"",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int DQUOTE(const char* s, Node* node) {
+	int toReturn = regexTest(s,"\"",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int HTAB(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"\t",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int HTAB(const char* s, Node* node) {
+	int toReturn = regexTest(s,"\t",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int OCTET(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"\0-ÿ",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int OCTET(const char* s, Node* node) {
+	int toReturn = regexTest(s,"\0-ÿ",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int SP(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s," ",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int SP(const char* s, Node* node) {
+	int toReturn = regexTest(s," ",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
-int VCHAR(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"[\\!-~]",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
+int VCHAR(const char* s, Node* node) {
+	int toReturn = regexTest(s,"[\\!-~]",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
 }
 
+int slash(const char* s, Node* node) {
+	int toReturn = regexTest(s,"/",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+
+int dot(const char* s, Node* node) {
+	int toReturn = regexTest(s,"\\.",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+
+int percent(const char* s, Node* node) {
+	int toReturn = regexTest(s,"%",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+
+int unreserved(const char* s, Node* node) {
+	int toReturn = regexTest(s,"^[A-Za-z0-9]|\\-|\\.|_|~$",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+int subDelims(const char* s, Node* node) {
+	int toReturn = regexTest(s,"^(\\!|\\$|&|'|\\(|\\)|\\*|\\+|,|;|\\=)",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+
+int colon(const char* s, Node* node) {
+	int toReturn = regexTest(s,"^:",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+
+int at(const char* s, Node* node) {
+	int toReturn = regexTest(s,"^@",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
+int tchar(const char* s, Node* node) {
+	int toReturn = regexTest(s,"(\\!|#|\\$|%|&|'|\\*|\\+|\\-|\\.|\\^|_|`|\\||~|[0-9A-Za-z])",1);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 1;}
+    	else removeNode(node);
+    }
+    return toReturn;
+}
 
 
 int OWS(const char *s, Node* node) {
-	result OWSaux(const char *s, Node* node) {return (result){(SP(s, NULL) || HTAB(s, NULL)), 1};}
-	result ret = etoile(OWSaux, s, 0, -1);
-	if(ret.boolean == TRUE)
-	{
-		createChild(node, ret.number, NULL);
-		for(int i = 0; i < ret.number; i++)
+	result OWSaux(const char *s, Node* node) {return (result){(SP(s, NULL) || HTAB(s, NULL)), 1};} //Fonction utilisée par etoile
+	result ret = etoile(OWSaux, s, 0, -1); //On appelle etoile, on regarde si il y a 0 ou plus occurrences
+	if(node != NULL){ //Si node vaut NULL, on renvoie juste le booléen
+		if(ret.boolean == TRUE) //Si le booléen est vrai, on crée les fils
 		{
-			if(SP(s+i, NULL))
-				addChild(node, "SP");
-			else if(HTAB(s+i, NULL))
-				addChild(node, "HTAB");
-			node->childList[i]->content = s + i;
-	        node->childList[i]->contentSize = 1;
+			createChild(node, ret.number, NULL); //etoile renvoie le nombre d'occurences
+			for(int i = 0; i < ret.number; i++)
+			{
+				if(SP(s+i, NULL)) {addChild(node, "SP"); SP(s+i, node->childList[0]);} //Si l'occurrence correspond à un espace, on rajoute le fils selon la fonction espace
+				else if(HTAB(s+i, NULL)) {addChild(node, "HTAB"); HTAB(s+i, node->childList[0]);} //Pareil pour HTAB
+			}
+			node->content = s; //On règle les valeurs du noeud père.
+			node->contentSize = ret.number;
+		} else {
+			removeNode(node); //Si le booléen est faux, on supprime le noeud
 		}
-		node->content = s;
-		node->contentSize = ret.number;
-	} else {
-		removeNode(node);
 	}
-	
 	return ret.boolean;
 }
 
 int RWS(const char *s, Node* node) {
 	result RWSaux(const char *s, Node* node) {return (result){(SP(s, NULL) || HTAB(s, NULL)), 1};}
 	result ret = etoile(RWSaux, s, 1, -1);
-	if(ret.boolean == TRUE)
-	{
-		createChild(node, ret.number, NULL);
-		for(int i = 0; i < ret.number; i++)
+	if(node != NULL){
+		if(ret.boolean == TRUE)
 		{
-			if(SP(s+i, NULL))
-				addChild(node, "SP");
-			else if(HTAB(s+i, NULL))
-				addChild(node, "HTAB");
-			node->childList[i]->content = s + i;
-	        node->childList[i]->contentSize = 1;
+			createChild(node, ret.number, NULL);
+			for(int i = 0; i < ret.number; i++)
+			{
+				if(SP(s+i, NULL)){addChild(node, "SP"); SP(s+i, node->childList[0]);}
+				else if(HTAB(s+i, NULL)){addChild(node, "HTAB"); HTAB(s+i, node->childList[0]);}
+			}
+			node->content = s;
+			node->contentSize = ret.number;
+		} else {
+			removeNode(node);
 		}
-		node->content = s;
-		node->contentSize = ret.number;
-	} else {
-		removeNode(node);
 	}
-	
 	return ret.boolean;
 }
 
 int BWS(const char *s, Node* node) {return OWS(s, node);}
 
 int CRLF(const char *s, Node* node){
-	int toReturn = FALSE;
-	createChild(node, 2, (char[][NAMESIZE]){"CR","LF"});
-    if(strlen(s) > 1) toReturn = (CR(s, node->childList[0]) && LF(s + 1, node->childList[1])); 
+	int toReturn = strlen(s) > 1 && (CR(s, node->childList[0]) && LF(s + 1, node->childList[1]));
+	if(node != NULL)
+	{
+		if(toReturn)
+		{
+			createChild(node, 2, (char[][NAMESIZE]){"CR","LF"});
+			CR(s, node->childList[0]);
+			LF(s + 1, node->childList[1]);
+		} else removeNode(node);
+	}
     return toReturn;
 }
 
-//Définitions que l'on retrouve souvent
-int tchar(const char *s, Node* node) {
-	int toReturn = FALSE;
-	if(regexTest(s,"(\\!|#|\\$|%|&|'|\\*|\\+|\\-|\\.|\\^|_|`|\\||~|[0-9A-Za-z])",1)){
-		if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-	}else{
-		removeNode(node);
-	}
-	return toReturn;
-}
 
 int token(const char *s, Node* node){
 	result tcharaux(const char *s, Node* node){return (result){tchar(s, node), 1};}
@@ -255,9 +258,7 @@ int token(const char *s, Node* node){
 		createChild(node, ret.number, NULL);
 		for(int i = 0; i < ret.number; i++)
 		{
-			addChild(node, "tchar");
-			node->childList[i]->content = s + i;
-	        node->childList[i]->contentSize = 1;
+			addChild(node, "tchar"); tchar(s+i, node->childList[i]);
 		}
 		node->content = s;
 		node->contentSize = ret.number;
@@ -268,106 +269,3 @@ int token(const char *s, Node* node){
 }
 
 int method(const char *s, Node* node) { return token(s, node); }
-
-//Début message HTTP
-int slash(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"/",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node);
-    }
-    return toReturn;
-}
-int dot(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"\\.",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
-    }
-    return toReturn;
-}
-
-int percent(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"%",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
-    }
-    return toReturn;
-}
-
-int unreserved(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"^[A-Za-z0-9]|\\-|\\.|_|~$",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
-    }
-    return toReturn;
-}
-
-int subDelims(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"^(\\!|\\$|&|'|\\(|\\)|\\*|\\+|,|;|\\=)",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
-    }
-    return toReturn;
-}
-int colon(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"^:",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
-    }
-    return toReturn;
-}
-int at(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"^@",1)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 1;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
-    }
-    return toReturn;
-}
--- src/highLevel.c
+++ src/highLevel.c
@@ -3,86 +3,89 @@
 #include "parser.h"
 #include "tree.h"
 
-
-int HTTPname(const char *s, Node* node){
-	int toReturn = FALSE;
-    if(regexTest(s,"HTTP",4)){
-        if(node != NULL)
-    	{
-    		node->content = s;
-        	node->contentSize = 4;
-    	}
-		toReturn =  TRUE;
-    } else {
-        removeNode(node); 
+int HTTPname(const char* s, Node* node) {
+	int toReturn = regexTest(s,"HTTP",4);
+    if(node != NULL)
+    {
+    	if(toReturn) {node->content = s; node->contentSize = 4;}
+    	else removeNode(node);
     }
     return toReturn;
 }
 
 
 int HTTPVersion(const char *s, Node* node){
-	if(node == NULL){
-		return(strlen(s) > 7 && (HTTPname(s, NULL) && slash(s+4, NULL) && DIGIT(s+5, NULL) && dot(s+6 , NULL) && DIGIT(s+7, NULL)));
-	}
-    createChild(node, 5, (char[][NAMESIZE]){"HTTP-name","/","DIGIT",".","DIGIT"});
-    if(strlen(s) > 7 && (HTTPname(s, node->childList[0]) && slash(s+4, node->childList[1]) && DIGIT(s+5, node->childList[2]) && dot(s+6 , node->childList[3]) && DIGIT(s+7, node->childList[4]))) {
-        node->content = s;
-        node->contentSize = 8;
-        return TRUE;
-    }
-    else {removeNode(node);return FALSE;}
+	int toReturn = (strlen(s) > 7 && (HTTPname(s, NULL) && slash(s+4, NULL) && DIGIT(s+5, NULL) && dot(s+6 , NULL) && DIGIT(s+7, NULL))); //On vérifie qu'on a bien tous les éléments
+	if(node != NULL) //Si le noeud est NULL, on renvoie juste le booléen
+	{
+		if(toReturn){ //Si le booléen est vrai, on crée les noeuds dans l'arbre
+			createChild(node, 5, (char[][NAMESIZE]){"HTTP-name","/","DIGIT",".","DIGIT"}); //on initialise les noeuds fils
+		    HTTPname(s, node->childList[0]); //on rajoute à chaque fils les bonnes infos
+		    slash(s+4, node->childList[1]);
+		    DIGIT(s+5, node->childList[2]);
+		    dot(s+6 , node->childList[3]);
+		    DIGIT(s+7, node->childList[4]);
+		    node->content = s; //On règle les valeurs du noeud père
+		    node->contentSize = 8;
+		    }else removeNode(node);	//Si le booléen est faux, on supprime le noeud
+		} 
+    return toReturn;
 }
 
 
 int pctEncoded(const char *s, Node* node){
-	if(node == NULL){
-		return(strlen(s) > 2 && (percent(s, NULL) && HEXDIG(s+1, NULL) && HEXDIG(s+2, NULL)));
-	}
-    createChild(node, 3, (char[][NAMESIZE]){"%","HEXDIG","HEXDIG"});
-    if(strlen(s) > 2 && (percent(s, node->childList[0]) && HEXDIG(s+1, node->childList[1]) && HEXDIG(s+2, node->childList[2]))) {
-        node->content = s;
-        node->contentSize = 3;
-        return TRUE;
+	int toReturn = (strlen(s) > 2 && (percent(s, NULL) && HEXDIG(s+1, NULL) && HEXDIG(s+2, NULL)));
+	if(node != NULL){
+		if(toReturn){
+			createChild(node, 3, (char[][NAMESIZE]){"%","HEXDIG","HEXDIG"});
+			percent(s, node->childList[0]);
+			HEXDIG(s+1, node->childList[1]);
+			HEXDIG(s+2, node->childList[2]);
+			node->content = s;
+	        node->contentSize = 3;
+		} else removeNode(node);
     }
-    else {removeNode(node);return FALSE;}
+    return toReturn;
 }
 
 result pchar(const char *s, Node* node){
-	if(node == NULL){
-		if(pctEncoded(s, NULL)) return((result){TRUE, 3});
-		else return((result){unreserved(s, NULL) || subDelims(s, NULL) || colon(s, NULL) || at(s,NULL),1});
-	}
-	createChild(node, 1, NULL);
-	if(pctEncoded(s, NULL)) {addChild(node, "pct-encoded"); pctEncoded(s, node->childList[0]);}
-	else if(subDelims(s, NULL)) {addChild(node, "sub-delims"); subDelims(s, node->childList[0]);}
-	else if(colon(s, NULL)) {addChild(node, ":"); colon(s, node->childList[0]);}
-	else if(at(s, NULL)) {addChild(node, "@"); at(s, node->childList[0]);}
-	else if(unreserved(s, NULL)) {addChild(node, "unreserved"); unreserved(s, node->childList[0]);}
-	else {removeNode(node); return (result){FALSE,0};}
-	node->content = s;
-	node->childList[0]->content = s;
-	node->contentSize = node->childList[0]->contentSize;
-	return (result){TRUE, node->contentSize};
+	result toReturn; //On renvoie un result car les fonctions suivantes auront besoin de connaître la taille du pchar
+	if(pctEncoded(s, NULL)) toReturn = ((result){TRUE, 3}); //Si on a un pct-encoded, la taille est de 3
+	else toReturn = ((result){unreserved(s, NULL) || subDelims(s, NULL) || colon(s, NULL) || at(s,NULL),1}); //Sinon la taille est de 1
+	if(node != NULL){ //Si la node est NULL, on renvoie juste le booléen(la taille sera inutile)
+		if(toReturn.boolean){ //Si le booléen est vrai, on crée le noeud dans l'arbre
+			createChild(node, 1, NULL);
+			if(pctEncoded(s, NULL)) {addChild(node, "pct-encoded"); pctEncoded(s, node->childList[0]);} //on regarde quel règle à été vérifiée puis on rajoute les bonnes infos au noeud
+			else if(subDelims(s, NULL)) {addChild(node, "sub-delims"); subDelims(s, node->childList[0]);}
+			else if(colon(s, NULL)) {addChild(node, ":"); colon(s, node->childList[0]);}
+			else if(at(s, NULL)) {addChild(node, "@"); at(s, node->childList[0]);}
+			else if(unreserved(s, NULL)) {addChild(node, "unreserved"); unreserved(s, node->childList[0]);}
+			node->content = s; //On règle les valeurs du noeud père
+			node->contentSize = node->childList[0]->contentSize;
+		} else removeNode(node); //Si le booléen est faux, on supprime le noeud père
+	}	
+	return toReturn;
 }
 
 result segment(const char *s, Node* node){
 	result ret = etoile(pchar, s, 0, -1);
-	if(node == NULL) return (result){ret.boolean, ret.number}; //pas le bon truc ?
-	int j = 0;
-	if(ret.boolean == TRUE)
+	if(node == NULL) return (result){ret.boolean, ret.number}; //A priori cette ligne est fausse, ret.number n'est pas la taille du segment mais le nombre d'élements qui le compose
+	if(node != NULL)
 	{
-		createChild(node, ret.number, NULL);
-		for(int i = 0; i < ret.number; i++)
+		int j = 0;
+		if(ret.boolean == TRUE)
 		{
-			addChild(node, "pchar"); pchar(s+j, node->childList[i]);
-			j += node->childList[i]->contentSize;
-		}
-		node->content = s;
-		int size = 0;
-		for(int i =0; i < node->childCount; i++) size += node->childList[i]->contentSize;
-		node->contentSize = size;
-	} else {
-		removeNode(node);
+			createChild(node, ret.number, NULL);
+			for(int i = 0; i < ret.number; i++)
+			{
+				addChild(node, "pchar"); pchar(s+j, node->childList[i]);
+				j += node->childList[i]->contentSize;
+			}
+			node->content = s;
+			int size = 0;
+			for(int i =0; i < node->childCount; i++) size += node->childList[i]->contentSize;
+			node->contentSize = size; //On calcule la taille du père à partir de la taille de ses fils
+		} else removeNode(node);
 	}
 	return (result){ret.boolean, node->contentSize};
 }
