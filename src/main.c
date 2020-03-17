10996971012c0ecfabf9ede570c799542a55f998
Benjamin LECOMTE <benjamin.lecomte@etu.esisar.grenoble-inp.fr>
2020-03-17 15:59:00 +0100

    RequestLine

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	}*/
 
 	Node* node = createNode("request-line");
-	if(requestLine("GE+ /where?q=now HTTP/1.1\r\n", node))
+	if(requestLine("GET /where?q=now HTTP/1.1\r\n", node))
 	{
 		printf("Ouep\n");
 		printTree(&node);
